707153fca991663c3a54bee097e52e36df62f764
mooreorless555 <christopher.v.moore@yale.edu>
2024-05-01 12:26:38 -0500

    Preserve decal object values when pasting

--- src/modules/decals/tools/DecalSelectTool.hx
+++ src/modules/decals/tools/DecalSelectTool.hx
@@ -63,7 +63,7 @@ class DecalSelectTool extends DecalTool
 				layerEditor.selected = [];
 				for (decal in DecalSelectTool.inClipboard)
 				{
-					var clone = new Decal(decal.position.clone(), decal.path, decal.texture, decal.origin.clone(), decal.scale.clone(), decal.rotation);
+					var clone = new Decal(decal.position.clone(), decal.path, decal.texture, decal.origin.clone(), decal.scale.clone(), decal.rotation, decal.values);
 					(cast layerEditor.layer:DecalLayer).decals.push(clone);
 					layerEditor.selected.push(clone);
 				}
